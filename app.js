886534baed5f214e8db07eeddc3cfced8a47ec7f
Ricardo Flores <hernandez.flores.rr@gmail.com>
2025-09-16 22:54:41 -0300

    Agrega funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
+
+//Agrega nombres desde el boton añadir se vincula el html
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+  
+  if (nombreAmigo === ""){
+    alert("Ingresa un nombre valido");
+  } else {
+    nombres.push(nombreAmigo);
+    console.log(nombres);
+  }
+
+    const lista = document.getElementById("listaAmigos");
+    const nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = nombreAmigo;
+        lista.appendChild(nuevoElemento);
+    limpiarCaja();
+
+}
