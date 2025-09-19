f1b088d7b2fa83c395da41e5f5dcf0e8e09c24a3
Ricardo Flores <hernandez.flores.rr@gmail.com>
2025-09-18 22:37:52 -0300

    Agrega funcion sortearAmigo, actualizar lista e ingresar nombres con enter

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
 
+function asignarTextoElemeno (elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
 //Agrega nombres desde el boton añadir se vincula el html
 function agregarAmigo() {
   let nombreAmigo = document.getElementById("amigo").value;
+
+  // Solo acepta letras y espacios
+  let soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
   
-  if (nombreAmigo === ""){
-    alert("Ingresa un nombre valido");
-  } else {
+  if (nombreAmigo === "" || !soloLetras.test(nombreAmigo) ){
+    alert("No se permiten números, caracteres especiales, ni campos vacíos. Ingrese un nombre válido.");
+    limpiarCaja();
+    return;
+  }
     nombres.push(nombreAmigo);
     console.log(nombres);
-  }
 
     const lista = document.getElementById("listaAmigos");
     const nuevoElemento = document.createElement("li");
         nuevoElemento.textContent = nombreAmigo;
         lista.appendChild(nuevoElemento);
     limpiarCaja();
-
 }
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = "";
+}
+
+function actualizarLista() {
+
+  const lista = document.getElementById("listaAmigos");
+
+  lista.innerHTML = "";
+
+  for (let i = 0; i < nombres.length; i++) {
+    
+    const nuevoElemento = document.createElement("li");
+    nuevoElemento.textContent = nombres[i];
+
+    lista.appendChild(nuevoElemento);
+  }
+}
+
+function sortearAmigo(){
+
+    if (nombres.length === 0) {
+        alert("No hay nombres para sortear");
+        return;
+      }
+
+   let sortear = Math.floor(Math.random() * nombres.length);
+   let nombreSeleccionado = nombres[sortear];
+   console.log(nombreSeleccionado);
+   asignarTextoElemeno("h2", `Tu amigo secreto es: ${nombreSeleccionado}`);
+} 
+
+document.getElementById("amigo").addEventListener("keydown", (event) => { 
+  if (event.key === "Enter"){
+    event.preventDefault();
+    agregarAmigo();
+  }
+})
